d34d95db4da81773a4f37add24801fc36dcc0f69
majka-meteor <maria.nemcova@outlook.com>
2015-10-29 17:20:09 +0100

    final commit conflict with double click/single click !!!

--- todo.js
+++ todo.js
@@ -1,3 +1,6 @@
+// Always try to avoid global variables
+// Strive to make functions reusable
+
 function updateItemStatus() {
  var cbId = this.id.replace("cb_", "") ; //function replace
 	//console.log(me);
@@ -6,31 +9,60 @@ function updateItemStatus() {
 	//itemText.innerText = "CLICKED";
 	 //css in js ! replace text-decoration
 	if (this.checked) {
-		itemText.style.textDecoration = "line-through";
+		itemText.className = "checked";
+		/*itemText.style.textDecoration = "line-through";
 		itemText.style.fontWeight = "800";
-		itemText.style.color = "#c00";
+		itemText.style.color = "#c00";*/
 	}
 	else {
-		itemText.style.textDecoration = "none";
+		itemText.className = "";
+		/*itemText.style.textDecoration = "none";
 		itemText.style.fontWeight = "400";
-		itemText.style.color = "#000";
+		itemText.style.color = "#000";*/
 
 	}
 }
 
+function renameItem(){
+	//this == span
+	//var cb_Id = this.id.replace("cb_", "") ;
+	var newText = prompt("What should this item be renamed to?");
+
+	if(!newText || newText =="" || newText ==" ") {
+		// (!itemText) to iste ako (itemText == false)
+		return false;
+	}
+	this.innerText = newText;
+}
+
+function removeItem(){
+	this.style.display = "none";
+	//this == span
+	/*var spanId = this.id.replace("item_", "") ;
+	document.getElementById("li_" + spanId).style.display = "none";*/
+	 //not delete but hide them
+}
+
 function addNewItem(list, itemText){
-	totalItems++;
+		//totalItems++;
 		 //<li><input type= "checkbox"/><span></span></li>
+	var date = new Date();
+	var id =  "" +date.getHours() + date.getMinutes()+ date.getSeconds() + date.getMilliseconds(); // we need unik number
+	// by adding "" string all numbers after are gonna ack like strings!
+
 	var listItem = document.createElement("li");
+	listItem.id = "li_" + id;
+	listItem.ondblclick = removeItem; // function on double click!
 
 	var checkBox = document.createElement("input");
 	checkBox.type = "checkbox";
-	checkBox.id = "cb_" + totalItems;
+	checkBox.id = "cb_" + id; //totalItems;
 	checkBox.onclick = updateItemStatus;
 
 	var span = document.createElement("span");
-	span.id = "item_" + totalItems;
+	span.id = "item_" + id; //totalItems;
 	span.innerText = itemText;
+	span.onclick =renameItem;
 
 	listItem.appendChild(checkBox);
 	listItem.appendChild(span);
@@ -38,20 +70,20 @@ function addNewItem(list, itemText){
 	//listItem.innerText = itemText; old code
 	list.appendChild(listItem); //check this plus global var
 
-
-
 }
-var totalItems = 0;
+
+//var totalItems = 0;
 var inItemText = document.getElementById("inItemText");
 inItemText.focus();
 
-var btnNew = document.getElementById("btnAdd");
+//var btnNew = document.getElementById("btnAdd");
 
 inItemText.onkeyup = function (event) {
 	// event.which 13 = ENTER
 	if(event.which == 13){
 		var itemText = inItemText.value;
 		//var itemText = event.which; //We used this 'which' property of the event onkeyup to know keycode for enter, it's 13 !!!
+
 		if(!itemText || itemText =="" || itemText ==" ") {
 		// (!itemText) to iste ako (itemText == false)
 			return false;

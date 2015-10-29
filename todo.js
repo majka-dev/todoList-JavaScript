03142dd264af1fd44aeb5e2bcbc7bddc16dc880a
majka-meteor <maria.nemcova@outlook.com>
2015-10-29 15:08:02 +0100

    todo list first commit without button

--- todo.js
+++ todo.js
@@ -6,15 +6,53 @@ function addNewItem(list, itemText){
 
 }
 
+var inItemText = document.getElementById("inItemText");
+inItemText.focus();
+
 var btnNew = document.getElementById("btnAdd");
-//btnNew.onclick = addNewItem; // bez zatvoriek lebo inak by sa ten alert objavil pri kazdom obnoveni stranky a takto sa objavi len ako prepojenie s funkciou napisanou hore
-btnNew.onclick = function () {
-	var itemText = prompt("What should be name new item?");
 
-	if(!itemText || itemText ==""){ // (!itemText) to iste ako (itemText == false)
-		return false; 
+inItemText.onkeyup = function (event) {
+	// event.which 13 = ENTER
+	if(event.which == 13){
+		var itemText = inItemText.value;
+		//var itemText = event.which; //We used this 'which' property of the event onkeyup to know keycode for enter, it's 13 !!!
+		if(!itemText || itemText =="" || itemText ==" ") {
+		// (!itemText) to iste ako (itemText == false)
+			return false;
+		}
+		addNewItem(document.getElementById("todoList"), itemText);
+		inItemText.focus();
+		inItemText.select();
+		// select text after enter -> no need to delete text before!
 	}
-
-	addNewItem(document.getElementById("todoList"), itemText);
 };
+
+//NOTE:
+//CODE OLDER WITH BUTTON !
+/*btnNew.onclick = function () {
+ var itemText = inItemText.value;
+ //var itemText = prompt("What should be name new item?"); predtym
+ if(!itemText || itemText =="" || itemText ==" ") { // (!itemText) to iste ako (itemText == false)
+ return false;
+ }
+ addNewItem(document.getElementById("todoList"), itemText);
+ };*/
+//btnNew.onclick = addNewItem; // bez zatvoriek lebo inak by sa ten alert
+// objavil pri kazdom obnoveni stranky a takto sa objavi len ako prepojenie s funkciou napisanou hore
+
+
+// With PROMPT
+
+/*inItemText.onkeyup = function (event) {
+	// event.which 13 = ENTER
+	if(event.which == 13){
+		var itemText = inItemText.value;
+		//var itemText = event.which; //'which' is the property of the event onkeyup  keycode for enter is 13 !!!
+		//var itemText = prompt("What should be name new item?"); predtym
+		if(!itemText || itemText =="" || itemText ==" ") { // (!itemText) to iste ako (itemText == false)
+			return false;
+		}
+		addNewItem(document.getElementById("todoList"), itemText);
+	}
+};*/
 
